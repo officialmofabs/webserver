26a115cf469f81db7e710185ddd166975c0b53fb
Moses Fabiyi <90117777+officialmofabs@users.noreply.github.com>
2025-04-10 12:19:34 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,4 +1,4 @@
 <?php
 require 'vendor/autoload.php';
-echo "<h2 style='text-align: center;'>Hello from container: " . gethostname() . "</h2>";
+echo "<h2 style='text-align: center;'>Hello from officialmofabs: " . gethostname() . "</h2>";
 phpinfo();
